aaf1a947a63989b159474188ffdff72ed3527d73
Eunice Montenegro <31542650+emontenegro410@users.noreply.github.com>
2017-11-08 22:03:23 -0600

    Edits

--- src/main/java/edu/luc/cs271/wordcount/Main.java
+++ src/main/java/edu/luc/cs271/wordcount/Main.java
@@ -6,7 +6,6 @@ import java.io.*;
 public class Main {
 
   public static void main(final String[] args) throws InterruptedException {
-
     // set up the scanner so that it separates words based on space and punctuation
     final Scanner input = new Scanner(System.in).useDelimiter("[^\\p{Alnum}]+");
 
@@ -21,19 +20,11 @@ public class Main {
     int sizeOfMap = mp.size();
     // 4. create an ArrayList of that size and
     ArrayList<Map.Entry<String, Integer>> sizes = new ArrayList<>(sizeOfMap);
-
     // 5. store the map's entries in it (these are of type Map.Entry<String, Integer>
-
     sizes.addAll(wordy.getCounts().entrySet());
-
     // 6. sort the ArrayList in descending order by count using Collections.sort and an instance of the provided comparator (after fixing the latter)
-
     Collections.sort(sizes, new DescendingByCount());
-
     // 7. print the (up to) ten most frequent words in the text
-    //   for(int i =0; i<9; i++){
-    // System.out.println("TO DO");
-    //  }
     if (wordy.getCounts().size() < 11) {
       for (int i = 0; i < sizes.size(); i++) {
         System.out.println(sizes.get(i));
